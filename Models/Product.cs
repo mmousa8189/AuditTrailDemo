33a92815b19260418159c51514de26ffb8ad12fe
Mohamed Mousa <mohamedamousa00@gmail.com>
2022-01-18 19:05:35 +0200

    Add project files.

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace AuditTrailDemo.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public int Rate { get; set; }
+    }
+}
